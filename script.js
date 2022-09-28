e0e06f26f080a95e98313e0b73f7303142741fe2
Abdelkrim <krimouv1995@gmail.com>
2022-09-28 23:11:04 +0100

    Implement operation functions

---
+++ script.js
@@ -0,0 +1,32 @@
+// operation functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b == 0) return "Error";
+    return a / b;
+}
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
+
+console.log(operate("/", 2, 0));
