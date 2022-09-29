ba968f300c359280c8b78befaacb582cd8ae5405
Abdelkrim <krimouv1995@gmail.com>
2022-09-29 21:22:27 +0100

    Implement code to display numbers

--- script.js
+++ script.js
@@ -29,4 +29,46 @@ function operate(operator, num1, num2) {
     }
 }
 
-console.log(operate("/", 2, 0));
+// populate display functions
+function displayDigits() {
+    const resultDisplay = document.querySelector(".result");
+    const digits = Array.from(document.querySelectorAll(".digit"));
+
+    digits.forEach(digit => {
+        digit.addEventListener("click", () => {
+            let text = digit.textContent;
+            resultDisplay.textContent += text;
+        });
+    });
+    // return resultDisplay.textContent;
+}
+
+displayDigits();
+
+function displayOperation() {
+    const resultDisplay = document.querySelector(".result");
+    const operationDisplay = document.querySelector(".operation-display");
+    const operators = Array.from(document.querySelectorAll(".operator"));
+
+    operators.forEach( operator => {
+        operator.addEventListener("click", (e) => {
+            if (e.target.textContent === "=") return;
+            else {
+                operationDisplay.textContent += resultDisplay.textContent + e.target.textContent;
+                resultDisplay.textContent = "";
+            }
+            
+            
+        });
+    });
+}
+
+displayOperation();
+
+
+function calculateResult() {
+    const resultDisplay = document.querySelector(".result");
+    const operationDisplay = document.querySelector(".operation-display");
+    
+}
+
