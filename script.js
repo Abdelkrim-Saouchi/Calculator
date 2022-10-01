13f9877fbb4ac793501cdf159413aaa7ba6c63de
Abdelkrim <krimouv1995@gmail.com>
2022-10-01 01:13:18 +0100

    Refacore code to fix some bugs

--- script.js
+++ script.js
@@ -38,10 +38,11 @@ function operate(operator, num1, num2) {
 function displayDigits() {
     const digits = Array.from(document.querySelectorAll(".digit"));
 
-    digits.forEach(digit => {
+    digits.forEach(digit => { //add digits to display
         digit.addEventListener("click", () => {
             let text = digit.textContent;
-            resultDisplay.textContent += text;
+            // resultDisplay.textContent += text;
+            operationDisplay.textContent += text;
         });
     });
 }
@@ -49,21 +50,47 @@ function displayDigits() {
 displayDigits();
 
 function displayOperation() {
+    // get operators in arrays
     const operators = Array.from(document.querySelectorAll(".operator"));
+    // counter to count number of operator in operation
     var counter = 0;
     operators.forEach( operator => {
         operator.addEventListener("click", (e) => {
-            
-            if (counter === 1) {
+            // if operator (except equal) is clicked before and clicked again fire calculations 
+            if (e.target.textContent !== "=" && (counter === 1 || isIncludeOperator(operationDisplay.textContent)) &&
+                 getOperationsArray(operationDisplay.textContent)[2] !== "") {
                 console.log("entered")
-                operationDisplay.textContent += resultDisplay.textContent;
-                resultDisplay.textContent = calculateResult(operationDisplay.textContent);
+                
+                
+                resultDisplay.textContent = calculateResult(getOperationsArray(operationDisplay.textContent));
+                operationDisplay.textContent = resultDisplay.textContent + e.target.textContent;
+                
                 counter = 0;
             }
+            // if equal was clicked and other operator was click before
+            else if (counter === 1 && e.target.textContent === "=") {
+                console.log("entered =");
+                resultDisplay.textContent = calculateResult(getOperationsArray(operationDisplay.textContent));
+                operationDisplay.textContent = resultDisplay.textContent;
+                counter = 0;
+            }
+            // add operator to display
             else {
-                operationDisplay.textContent = resultDisplay.textContent + e.target.textContent;
-                resultDisplay.textContent = "";
-                counter++;
+                console.log("entered else");
+                if (getOperationsArray(operationDisplay.textContent).length === 3 && getOperationsArray(operationDisplay.textContent) !== "") {
+                    resultDisplay.textContent = calculateResult(getOperationsArray(operationDisplay.textContent));
+                    operationDisplay.textContent = resultDisplay.textContent;
+                    counter = 0;
+                }
+               else {
+                    if (e.target.textContent === "=") return; //prevent addition of equal to display
+                    operationDisplay.textContent += e.target.textContent;
+                    resultDisplay.textContent = "";
+                    counter++; //indicate that operator is clicked
+               }
+                // const array = getOperationsArray(operationDisplay.textContent);
+                // console.log(typeof array[2]);
+                
             }
                 
             
@@ -75,7 +102,8 @@ function displayOperation() {
 displayOperation();
 
 
-function calculateResult(operationString) {
+// function that transforms string of operation to array
+function getOperationsArray(operationString) {
     let expression = operationString;
     let copy = expression;
 
@@ -90,8 +118,21 @@ function calculateResult(operationString) {
     }
 
     console.log(result);
+    return result;
     
-    return `${operate(result[1], +result[0], +result[2])}`;
     
 }
 
+//Do calculations
+function calculateResult(array) {
+    return `${operate(array[1], +array[0], +array[2])}`;
+}
+
+// check if string of operations contains operator
+function isIncludeOperator(operationString) {
+    if (operationString.includes("x")) return true;
+    else if (operationString.includes("/")) return true;
+    else if (operationString.includes("+")) return true;
+    else if (operationString.includes("-")) return true;
+    else return false;
+}
